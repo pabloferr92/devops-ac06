a1ffb8f91774cfa44c609dfce6c9545add55d581
nardotto89 <dd-nardoto@bol.com.br>
2018-05-17 14:47:02 -0300

    correção da views, criação models vestibular e curso e modificações nos arquivos tamplates

--- app/views.py
+++ app/views.py
@@ -5,8 +5,9 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from app.models import Curso
+from app.models import Curso, Vestibular
 from datetime import datetime
+
 
 def home(request):
     """Renders the home page."""
@@ -14,12 +15,13 @@ def home(request):
     return render(
         request,
         'app/index.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        })
+        context_instance=RequestContext(request,
+                                        {
+                                            'title': 'Home Page',
+                                            'year': datetime.now().year,
+                                        })
     )
+
 
 def contact(request):
     """Renders the contact page."""
@@ -27,13 +29,14 @@ def contact(request):
     return render(
         request,
         'app/contact.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Contato',
-            'message':'Entre em contato conosco',
-            'year':datetime.now().year,
-        })
+        context_instance=RequestContext(request,
+                                        {
+                                            'title': 'Contato',
+                                            'message': 'Entre em contato conosco',
+                                            'year': datetime.now().year,
+                                        })
     )
+
 
 def about(request):
     """Renders the about page."""
@@ -41,25 +44,31 @@ def about(request):
     return render(
         request,
         'app/about.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'About',
-            'message':'Gerenciador de vestibulares',
-            'year':datetime.now().year,
-        })
+        context_instance=RequestContext(request,
+                                        {
+                                            'title': 'About',
+                                            'message': 'Gerenciador de vestibulares',
+                                            'year': datetime.now().year,
+                                        })
     )
+
 
 def cadastro_cursos(request):
     assert isinstance(request, HttpRequest)
     return render(
         request,
         'app/cadastro_cursos.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Cadastro de cursos',
-#            'cursos': ['ADS' , 'SI', 'CC'],
-            'cursos': Curso.objects.all(),
-            'year':datetime.now().year,
-        })
+        context_instance=RequestContext(request,
+                                        {
+                                            'title': 'Cadastro de cursos',
+                                            #            'cursos': ['ADS' , 'SI', 'CC'],
+                                            'cursos': Curso.objects.all(),
+                                            'year': datetime.now().year,
+                                        })
     )
 
+
+def cadastro_vestibulares(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, "app/cadastro_vestibulares.html", context_instance=RequestContext(request, {
+        "title": "Cadastro de vestibulares", "vestibulares": Vestibular.objects.all(), "year": datetime.now().year, }))
